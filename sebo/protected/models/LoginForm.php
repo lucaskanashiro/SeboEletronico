dd655b7779130bdfd68cdb87b88588992f3c76c4
lucaskanashiro <lucas-kanashiro@hotmail.com>
2013-05-31 10:31:23 -0300

    Update LoginForm.php

--- sebo/protected/models/LoginForm.php
+++ sebo/protected/models/LoginForm.php
@@ -1,10 +1,6 @@
 <?php
 
-/**
- * LoginForm class.
- * LoginForm is the data structure for keeping
- * user login form data. It is used by the 'login' action of 'SiteController'.
- */
+
 class LoginForm extends CFormModel
 {
 	public $username;
@@ -13,26 +9,20 @@ class LoginForm extends CFormModel
 
 	private $_identity;
 
-	/**
-	 * Declares the validation rules.
-	 * The rules state that username and password are required,
-	 * and password needs to be authenticated.
-	 */
+	
 	public function rules()
 	{
 		return array(
-			// username and password are required
+		
 			array('username, password', 'required'),
-			// rememberMe needs to be a boolean
+		
 			array('rememberMe', 'boolean'),
-			// password needs to be authenticated
+		
 			array('password', 'authenticate'),
 		);
 	}
 
-	/**
-	 * Declares attribute labels.
-	 */
+
 	public function attributeLabels()
 	{
 		return array(
@@ -40,10 +30,7 @@ class LoginForm extends CFormModel
 		);
 	}
 
-	/**
-	 * Authenticates the password.
-	 * This is the 'authenticate' validator as declared in rules().
-	 */
+
 	public function authenticate($attribute,$params)
 	{
 		if(!$this->hasErrors())
@@ -54,10 +41,7 @@ class LoginForm extends CFormModel
 		}
 	}
 
-	/**
-	 * Logs in the user using the given username and password in the model.
-	 * @return boolean whether login is successful
-	 */
+	
 	public function login()
 	{
 		if($this->_identity===null)
@@ -67,7 +51,7 @@ class LoginForm extends CFormModel
 		}
 		if($this->_identity->errorCode===UserIdentity::ERROR_NONE)
 		{
-			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 days
+			$duration=$this->rememberMe ? 3600*24*30 : 0; // 30 dias
 			Yii::app()->user->login($this->_identity,$duration);
 			return true;
 		}
