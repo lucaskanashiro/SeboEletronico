4c12db57dac5409399b4c94e23a8b5fe357b8ac0
lucaskanashiro <lucas-kanashiro@hotmail.com>
2013-05-31 10:30:02 -0300

    Update ContactForm.php

--- sebo/protected/models/ContactForm.php
+++ sebo/protected/models/ContactForm.php
@@ -13,11 +13,11 @@ class ContactForm extends CFormModel
 	public function rules()
 	{
 		return array(
-			// name, email, subject and body are required
+		
 			array('name, email, subject, body', 'required'),
-			// email has to be a valid email address
+		
 			array('email', 'email'),
-			// verifyCode needs to be entered correctly
+		
 			array('verifyCode', 'captcha', 'allowEmpty'=>!CCaptcha::checkRequirements()),
 		);
 	}
