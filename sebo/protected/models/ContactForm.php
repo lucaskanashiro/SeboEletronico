46510ba13b506be1bef397fadd3c882ebc7e5128
lucaskanashiro <lucas-kanashiro@hotmail.com>
2013-05-31 10:29:39 -0300

    Update ContactForm.php

--- sebo/protected/models/ContactForm.php
+++ sebo/protected/models/ContactForm.php
@@ -1,10 +1,6 @@
 <?php
 
-/**
- * ContactForm class.
- * ContactForm is the data structure for keeping
- * contact form data. It is used by the 'contact' action of 'SiteController'.
- */
+
 class ContactForm extends CFormModel
 {
 	public $name;
@@ -13,9 +9,7 @@ class ContactForm extends CFormModel
 	public $body;
 	public $verifyCode;
 
-	/**
-	 * Declares the validation rules.
-	 */
+
 	public function rules()
 	{
 		return array(
@@ -28,11 +22,7 @@ class ContactForm extends CFormModel
 		);
 	}
 
-	/**
-	 * Declares customized attribute labels.
-	 * If not declared here, an attribute would have a label that is
-	 * the same as its name with the first letter in upper case.
-	 */
+
 	public function attributeLabels()
 	{
 		return array(
