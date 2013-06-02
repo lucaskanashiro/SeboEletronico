25f41ffecc38c47e637179603b203dc90374e12f
Jessica Suzuki <suzuki_jessica@hotmail.com>
2013-06-02 18:05:06 -0300

    Comeco classe dentro de login

--- sebo/protected/views/usuario/tabelaUsuarios.php
+++ sebo/protected/views/usuario/tabelaUsuarios.php
@@ -1,3 +1,26 @@
-<?php
-    
-?>
+<html>
+<head>
+
+</head>
+
+<body>
+<div id="mainmenu">
+<?php $this->widget('zii.widgets.CMenu',array(
+'items'=>array(
+array('label'=>'Home', 'url'=>array('/site/index')),
+array('label'=>'Alterar Cadastro', 'url'=>array('/usuario/ALTERAAAAAAAA')),
+array('label'=>'Excluir Cadastro', 'url'=>array('/usuario/EXCLUIIIIIIR')),
+array('label'=>'Logout ', 'url'=>array('/site/logout'), 'visible'=>Yii::app()->user)
+),
+)); ?>
+</div>
+
+<br/>
+<br/>
+
+
+<img src="<?php echo Yii::app()->request->baseUrl; ?>/img/sejaBemVindo.jpg" class="img"/>
+
+
+</body>
+</html>
