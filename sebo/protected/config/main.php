0c7ed090552e944c838c4ef3eca10d28c4f35433
Jessica Suzuki <suzuki_jessica@hotmail.com>
2013-06-02 14:31:05 -0300

    Refatorado

--- sebo/protected/config/main.php
+++ sebo/protected/config/main.php
@@ -1,20 +1,15 @@
 <?php
 
-// uncomment the following to define a path alias
-// Yii::setPathOfAlias('local','path/to/local-folder');
-
-// This is the main Web application configuration. Any writable
-// CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'Sebo Eletronico',
 	'sourceLanguage'=>'pt_br',
 	'language'=>'pt_br',
 
-	// preloading 'log' component
+	
 	'preload'=>array('log'),
 
-	// autoloading model and component classes
+	
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
@@ -22,24 +17,20 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
+		
 		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
 			'password'=>'sebo',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
 		
 	),
 
-	// application components
 	'components'=>array(
 		'user'=>array(
-			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
-		// uncomment the following to enable URLs in path-format
 		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
@@ -50,11 +41,6 @@ return array(
 			),
 		),
 		
-		/*'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),*/
-		// uncomment the following to use a MySQL database
-		
 		'db'=>array(
 			'connectionString' => 'pgsql:host=localhost;dbname=sebo',
 			'emulatePrepare' => true,
@@ -64,7 +50,6 @@ return array(
 		),
 		
 		'errorHandler'=>array(
-			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
 		),
 		'log'=>array(
@@ -74,20 +59,12 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
-				// uncomment the following to show log messages on web pages
-				/*
-				array(
-					'class'=>'CWebLogRoute',
-				),
-				*/
+				
 			),
 		),
 	),
 
-	// application-level parameters that can be accessed
-	// using Yii::app()->params['paramName']
 	'params'=>array(
-		// this is used in contact page
 		'adminEmail'=>'macario.junior@gmail.com',
 	),
 );
--- sebo/protected/config/test.php
+++ sebo/protected/config/test.php
@@ -7,11 +7,6 @@ return CMap::mergeArray(
 			'fixture'=>array(
 				'class'=>'system.test.CDbFixtureManager',
 			),
-			/* uncomment the following to provide test database connection
-			'db'=>array(
-				'connectionString'=>'DSN for test database',
-			),
-			*/
 		),
 	)
 );
