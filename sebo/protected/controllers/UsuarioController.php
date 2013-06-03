a4c91f10fa2d9ece37dc6f38b2b0733a38e1c5b4
MacSoares <macario.junior@gmail.com>
2013-06-03 13:10:11 -0300

    atualizando versao

--- sebo/protected/controllers/UsuarioController.php
+++ sebo/protected/controllers/UsuarioController.php
@@ -6,7 +6,15 @@ class UsuarioController extends Controller
 	{
 		$this->render('index');
 	}
-
+        
+        public function actionAlterar(){
+            $this->render('alteraUsuario');
+        }
+        
+        public function actionDeletar(){
+            $this->render('excluiUsuario');
+        }
+    
 	public function actionCadastrar(){
             
             $nome = $_POST['nome'];
@@ -16,6 +24,25 @@ class UsuarioController extends Controller
             
             
             Usuario::Cadastrar($nome, $email, $telefone, $senha);
+        ?>
+        <script language = "Javascript">
+            alert("Cadastro efetuado com sucesso");
+        </script>
+        <script language = "Javascript">
+        window.location="<?php echo Yii::app()->request->baseUrl; ?>";
+        </script>
+<?php     
+        }
+
+        public function actionAlterarCadastro(){
+            
+            $nome = $_POST['nome'];
+            $email = $_POST['email'];
+            $telefone = $_POST['telefone'];
+            $senha = $_POST['senha'];
+            
+            
+            Usuario::Alterar($nome, $email, $telefone, $senha);
         ?>
         <script language = "Javascript">
             alert("Cadastro efetuado com sucesso");
