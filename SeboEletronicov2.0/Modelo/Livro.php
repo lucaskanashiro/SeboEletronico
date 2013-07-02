017c38a0da4f1d0ae1d50354cff93655170fd30c
jgbs <joaogabrieldebrittoesilva@gmail.com>
2013-07-02 18:38:28 -0300

    Criacao da classe modelo Livro

--- SeboEletronicov2.0/Modelo/Livro.php
+++ SeboEletronicov2.0/Modelo/Livro.php
@@ -1,7 +1,54 @@
 <?php
 
 class Livro {
-    //put your code here
-}
+    private $titulo;
+    private $autor;
+    private $genero;
+    private $tipoDeOperacao;//compra, venda, troca
+    
+    function __construct($titulo, $autor, $genero, $tipoDeOperacao) {
+        $this->titulo = $titulo;
+        $this->autor = $autor;
+        $this->genero = $genero;
+        $this->tipoDeOperacao = $tipoDeOperacao;
+    }
 
+    
+    public function getTitulo() {
+        return $this->titulo;
+    }
+
+    public function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    public function getAutor() {
+        return $this->autor;
+    }
+
+    public function setAutor($autor) {
+        $this->autor = $autor;
+    }
+
+    public function getGenero() {
+        return $this->genero;
+    }
+
+    public function setGenero($genero) {
+        $this->genero = $genero;
+    }
+
+    public function getTipoDeOperacao() {
+        return $this->tipoDeOperacao;
+    }
+
+    public function setTipoDeOperacao($tipoDeOperacao) {
+        $this->tipoDeOperacao = $tipoDeOperacao;
+    }
+
+
+
+    
+    
+}
 ?>
