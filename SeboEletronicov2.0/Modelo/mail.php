7929be24954c245e28b13873639bd276471ce708
Caique <caiquepereira@gmail.com>
2013-07-03 19:29:58 -0300

    Adicionando classe mail php que dispara email para o vendedor do livro

---
+++ SeboEletronicov2.0/Modelo/mail.php
@@ -0,0 +1,17 @@
+<?php
+$destinatario = 'levi.moraes@yahoo.com.br';
+$mensagem =
+" <a href ="www.google.com.br"> 
+Visite a pagina do google </a>
+<h1> Hello World </h1>";
+
+
+
+$subject= 'Existe uma pessoa interessada no seu Livro - Sebo Eletronico'; // Assunto.
+$to= $destinatario; // Para.
+$body= $mensagem; // corpo do texto.
+if (mail($to,$subject,$body,"Content-Type: text/html"))
+echo 'e-mail enviado com sucesso!';
+else
+echo 'e-mail não enviado!';
+?>
