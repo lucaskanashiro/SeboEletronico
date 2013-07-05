99ef7d9c013b76b30e1cc3c759df2b812241470b
levimoraes <levi.moraes@yahoo.com.br>
2013-07-04 18:40:35 -0700

    Disparo de Email correto, ja com Layout

--- SeboEletronicov2.0/Modelo/mail.php
+++ SeboEletronicov2.0/Modelo/mail.php
@@ -1,10 +1,23 @@
 <?php
-$destinatario = 'levi.moraes@yahoo.com.br';
-$mensagem =
-' <a href ="www.google.com.br"> 
-Visite a pagina do google </a>
-<h1> Hello World </h1>';
+include 'Usuario.php';
 
+$destinatario = Usuario::getEmail();
+$mensagem ='<html>
+				<body>
+					<table background = "http://i.imgur.com/GX69Php.jpg" height = "800" width=" 650" padding-top = "300" padding-right= "100" padding-bottom ="300" padding-left= "100">
+							
+						<tr>
+							<td valign="top">
+								<br><br><br><br><br><br><br><br><br><br><br><br>
+								<br><font color = "#FFFFFF" size = "6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nome do Livro: </br></font>
+								<br><font color = "#FFFFFF" size = "6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nome do Comprador: </br></font>
+								<br><font color = "#FFFFFF" size = "6">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Email: </br></font>                          
+							</td>
+						</tr>
+					</table>		
+				</body>
+
+				</html>';
 
 
 $subject= 'Existe uma pessoa interessada no seu Livro - Sebo Eletronico'; // Assunto.
@@ -13,5 +26,5 @@ $body= $mensagem; // corpo do texto.
 if (mail($to,$subject,$body,"Content-Type: text/html"))
 echo 'e-mail enviado com sucesso!';
 else
-echo 'e-mail n�o enviado!';
+echo 'e-mail nao enviado!';
 ?>
