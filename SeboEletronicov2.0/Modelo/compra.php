ff3a1b3e81b186fbecd5acca502dd4e3377e99a8
levimoraes <levi.moraes@yahoo.com.br>
2013-07-07 18:26:58 -0700

    Adicionar Classe de Compra

---
+++ SeboEletronicov2.0/Modelo/compra.php
@@ -0,0 +1,22 @@
+<?php
+
+class faz{
+	public function fazer () {
+	
+		
+//<input type=button value="sadfc it now" onClick="alert('Hello from JavaScript!')">
+
+		return TRUE;
+	
+	}
+}
+	//Colocar Arquivos AKI
+		
+		
+		}
+	?> 
+	
+	<input type=button value="Compre" onClick="alert('Compra feita com Sucesso')">
+
+	<a id="myLink" href="add_carrinho.php" onclick="fazer();">Comprar</a>
+		
