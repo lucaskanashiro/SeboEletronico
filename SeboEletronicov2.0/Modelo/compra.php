31ec58e5c99f13d6a585d10fbfafb3933221d1e7
levimoraes <levi.moraes@yahoo.com.br>
2013-07-07 18:30:00 -0700

    Continuacao da Classe Compra

--- SeboEletronicov2.0/Modelo/compra.php
+++ SeboEletronicov2.0/Modelo/compra.php
@@ -10,7 +10,20 @@ class faz{
 	
 	}
 }
-	//Colocar Arquivos AKI
+	include "conexao_bd.inc";
+	if(!$bd) die ("<h1>Falha no bd </h1>");
+		
+		
+		$strSQL = "SELECT * FROM livro WHERE tipo_operacao = 'COMPRA' ";
+						
+			
+		$rs = mysql_query($strSQL);
+		
+		while($row = mysql_fetch_array($rs)) {
+	   
+		echo $row['titulo_livro'] . "<br />";
+		echo $row['estado_conserv'] . "<br />";
+		//echo $row['tipo_operacao'] . "<br /> <br />" ;
 		
 		
 		}
