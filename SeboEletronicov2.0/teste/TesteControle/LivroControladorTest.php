9e950a66e065db4853d944766228e611e70eeab9
Caique <caiquepereira@gmail.com>
2013-07-14 14:36:46 -0300

    Adicionando metodos de teste da Livro Controladora na classe LivroControladorTest

--- SeboEletronicov2.0/teste/TesteControle/LivroControladorTest.php
+++ SeboEletronicov2.0/teste/TesteControle/LivroControladorTest.php
@@ -4,15 +4,50 @@ require_once "../SeboEletronico/SeboEletronicov2.0/Controle/LivroControlador.php
 
 class LivroControladorTest extends PHPUnit_Framework_TestCase{
 
-	protected $control;
+	protected $livroControlador;
 
 	protected function setUp(){
-		$this->control = new LivroControlador();
+		$this->livroControlador = new LivroControlador();
 	}
 
 	protected function tearDown(){
-		unset($this->control);
+		unset($this->livroControlador);
 	}
+
+        
+        public function salvaLivroTeste (){
+            $retorno= $this->livroControlador->salvaLivro('calculo 1', 'Thomas', 'engenharia', 2, 'editora teste', True, True, 'novo');
+            $this->assertTrue($retorno);
+            
+        }
+        
+        
+        public function pesquisaLivroTeste(){
+           
+          //ainda a fazer  
+            
+        }
+        
+        public function getLivroByIdTeste(){
+            $retorno=  $this->livroControlador->getLivroById('1');
+            
+            if($retorno===false){
+                $this->assertFalse($retorno);
+            }  else {
+                $this->assertNotEmpty($retorno);
+            }
+        }
+     
+ 
+        public function deletaLivroTeste(){
+            $retorno=  $this->livroControlador->deletaLivro();
+            $this->assertTrue($retorno);
+            
+        }
+        
+        
+        
+        
 }
 
 ?>
