b3387f863e2c075244e0e928584c3bc9a29f1473
Levi <levis@levis.notebook>
2013-07-15 16:45:54 -0300

    Resolveu problema 1 = booleano

--- SeboEletronicov2.0/Dao/LivroDao.php
+++ SeboEletronicov2.0/Dao/LivroDao.php
@@ -55,23 +55,11 @@ class LivroDao {
     }
     
     public function alteraLivro($titulo, $autor, $genero, $edicao, $editora, $venda, $troca, $estado, $descricao, $id, $id_usuario){
-        //echo "livrodao</br>";
-        //var_dump($genero);
-        //var_dump($id_usuario);
-        //var_dump($id);
-        //exit;
-        
-        //$sql = "UPDATE livro SET id_dono = '".$id_usuario."', titulo_livro = '".$titulo."', autor = '".$autor."', genero = '".$genero."', edicao = '".$edicao."', 
-        //   editora = '".$editora."', venda = '".$venda."', troca = '".$troca."', estado_conserv = '".$estado."', descricao = '".$descricao."' 
-        //        WHERE id_livro = '".$id."'";
+
         $sql = "UPDATE livro SET id_dono = '".$id_usuario."', titulo_livro = '".$titulo."', editora = '".$editora."', autor = '".$autor."', edicao = '".$edicao."', genero = '".$genero."', estado_conserv = '".$estado."', descricao_livro = '".$descricao."', venda = '".$venda."', troca = '".$troca."' WHERE id_livro = '".$id."'";
         $livro = mysql_query($sql);
         
-        if($livro){
-            return 1;
-        }else{
-            return 0;
-        }
+        return $livro;
     }   
 }
 ?>
